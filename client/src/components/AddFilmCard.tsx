b47d664e9cc2fbb63ff4ec0595c08eba289de624
Dmitry Eshman <bakko7821@gmail.com>
2025-09-27 17:27:06 +0300

    commit 5.6.1

--- client/src/components/AddFilmCard.tsx
+++ client/src/components/AddFilmCard.tsx
@@ -82,65 +82,84 @@ export default function AddFilmCard() {
   return (
     <>
       <p className="titleText">Добавление рецезнии</p>
-      <div>
-        <div className="floating-input">
-          <input
-            type="search"
-            id="search"
-            name="search"
-            className="searchInput"
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-            placeholder="Поиск фильма по названию"
-            required
-          />
-          <label htmlFor="email">Поиск фильма по названию</label>
+      <div className="findOrAddFilmBox flex-column">
+        <p>Попробуйте найти фильм в списке фильмов, если его нету. Вставьте ссылку на страницу фильма из сервиса <a href="https://www.kinopoisk.ru/">Кинопоиск</a></p>
+        <div className="inputsBox flex-center">
+          <div className="floating-input">
+            <input
+              type="search"
+              id="search"
+              name="search"
+              className="searchInput"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              placeholder="Поиск фильма по названию"
+              required
+            />
+            <label htmlFor="email">Поиск фильма по названию</label>
+          </div>
+          {searchResults.length > 0 && (
+            <ul className="searchResults flex-column">
+              {searchResults.map((f, idx) => (
+                <li key={idx} className="searchFilmCard flex-center">
+                  {f.poster && <img src={f.poster} alt="poster"/>}
+                  <p>{f.title}</p>
+                  <p>({f.year})</p>
+                </li>
+              ))}
+            </ul>
+          )}
+          <p>Или</p>
+          <div className="addFillmFromLink flex-center">
+            <div className="floating-input">
+            <input
+              className="addFilmInput"
+              value={url}
+              onChange={e => setUrl(e.target.value)}
+              placeholder="Вставьте ссылку на Кинопоиск"
+            />
+            <label htmlFor="email">Вставьте ссылку на Кинопоиск</label>
+            </div>
+            <button onClick={handleParseAndAdd}>
+              <img src="../../public/images/add2.svg" alt="Добавить" />
+            </button>
+          </div>
+          
+          {error && <div className="notificationMessage error flex-center">
+              <img src="../../public/images/error.svg" alt="" />
+              <p>{error}</p>
+            </div>}
+          {message && <div className="notificationMessage message flex-center">
+              <img src="../../public/images/successful.svg" alt="" />
+              <p>{message}</p>
+            </div>}
         </div>
-        {searchResults.length > 0 && (
-          <ul className="searchResults">
-            {searchResults.map((f, idx) => (
-              <li key={idx} className="searchFilmCard flex-center">
-                {f.poster && <img src={f.poster} alt="poster"/>}
-                <p>{f.title}</p>
-                <p>({f.year})</p>
-              </li>
-            ))}
-          </ul>
-        )}
-        <div style={{ marginTop: 20 }}>
-          <input
-            value={url}
-            onChange={e => setUrl(e.target.value)}
-            placeholder="Вставьте ссылку на Кинопоиск"
-            style={{ width: 400 }}
-          />
-          <button onClick={handleParseAndAdd}>
-            <img src="../../public/images/add2.svg" alt="Добавить" />
-          </button>
-        </div>
-
-        {error && <div className="errorMessage">{error}</div>}
-        {message && <div className="notificationMessage">{message}</div>}
-
-        {film && (
-          <div style={{ marginTop: 20 }}>
-            {film.poster && <img src={film.poster} alt="poster" />}
-            <div className="filmTextInfoBox">
-              <div className="titleBox">
-                <p>Название:</p>
-                <p>{film.title} {film.year ? `(${film.year})` : ""}</p>
-              </div>
-              <div className="genresBox">
-                <p>Жанры:</p>
-                <div className="genserList">
-                  {film.genres?.map((genre, index) => (
-                    <div key={index} className="genreCard">{genre}</div>
-                  ))}
+      </div>
+      <div className="bottomBox flex-center">
+        <div className="choosedFilmInfoBox">
+          {film && (
+            <div>
+              {film.poster && <img src={film.poster} alt="poster" />}
+              <div className="filmTextInfoBox">
+                <div className="titleBox">
+                  <p>Название:</p>
+                  <p>{film.title} {film.year ? `(${film.year})` : ""}</p>
+                </div>
+                <div className="genresBox">
+                  <p>Жанры:</p>
+                  <div className="genserList">
+                    {film.genres?.map((genre, index) => (
+                      <div key={index} className="genreCard">{genre}</div>
+                    ))}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        )}
+          )}
+        </div>
+        <div className="userReviewBox">
+
+        </div>
       </div>
     </>
   );
